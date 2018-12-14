3a39cf5147b77082cb0328942c4fb1a35f92dced
Anna157 <45663551+Anna157@users.noreply.github.com>
2018-12-14 10:08:27 -0500

    Update triangles.py

--- triangles.py
+++ triangles.py
@@ -1,4 +1,5 @@
-a = int(input("Choose the first side of the triangle"))
-b = int(input("Choose the second side of the triangle"))
+import math
+a = int(input("Choose the first side of the triangle: "))
+b = int(input("Choose the second side of the triangle: "))
 c = math.sqrt((a*a) + (b*b))
 print (f¨The hypotenuse of this triangle is equal to {c}")
